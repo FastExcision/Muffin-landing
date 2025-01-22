27de99231e598cf77c389dd4344477bb7569a7a1
Fastexcision <atlee@yandex.ru>
2025-01-22 13:43:16 +0300

    feat: add import section in html doc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import './index.scss';
+import './index.html';
+import './modules/importHtml';
---
+++ src/modules/importHtml.js
@@ -0,0 +1,6 @@
+/* global document */
+import header from '../header.html';
+import firstPage from '../first-page.html';
+
+document.getElementById('first-page').innerHTML = firstPage;
+document.getElementById('header').innerHTML = header;
