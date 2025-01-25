7a444e706dc56e9e7a9b5608335789d3d188d82b
Fastexcision <atlee@yandex.ru>
2025-01-25 15:50:24 +0300

    feat: rubber title page.

--- src/modules/importHtml.js
+++ src/modules/importHtml.js
@@ -1,6 +1,22 @@
 /* global document */
 import header from '../header.html';
-import firstPage from '../first-page.html';
+import titlePage from '../title-page.html';
+import childhoodPage from '../childhood-page.html';
+import oneDayPage from '../one-day-page.html';
 
-document.getElementById('first-page').innerHTML = firstPage;
-document.getElementById('header').innerHTML = header;
+const headerElement = document.getElementById('header');
+const titlePageElement = document.getElementById('title-page');
+const childhoodPageElement = document.getElementById('childhood-page');
+const oneDayPageElement = document.getElementById('one-day-page');
+
+const importPagesArray = [header, titlePage, childhoodPage, oneDayPage];
+const elementsArray = [headerElement, titlePageElement, childhoodPageElement, oneDayPageElement];
+const loadSection = () => {
+  for (let i = 0; i < elementsArray.length; i += 1) {
+    if (elementsArray[i]) {
+      elementsArray[i].innerHTML = importPagesArray[i];
+    }
+  }
+};
+
+loadSection();
