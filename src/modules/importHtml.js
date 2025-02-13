8cbb6035997ed1002af76c052d6048932266a1b3
Fastexcision <atlee@yandex.ru>
2025-02-13 20:36:17 +0300

    feat: bad-days-page full complete

--- src/modules/importHtml.js
+++ src/modules/importHtml.js
@@ -3,18 +3,27 @@ import header from '../pages/header.html';
 import titlePage from '../pages/title-page.html';
 import childhoodPage from '../pages/childhood-page.html';
 import oneDayPage from '../pages/one-day-page.html';
+import badDaysPage from '../pages/bad-days-page.html';
 
 const headerElement = document.getElementById('header');
 const titlePageElement = document.getElementById('title-page');
 const childhoodPageElement = document.getElementById('childhood-page');
 const oneDayPageElement = document.getElementById('one-day-page');
+const badDaysPageElement = document.getElementById('bad-days-page');
 
-const importPagesArray = [header, titlePage, childhoodPage, oneDayPage];
+const importPagesArray = [
+  header,
+  titlePage,
+  childhoodPage,
+  oneDayPage,
+  badDaysPage,
+];
 const elementsArray = [
   headerElement,
   titlePageElement,
   childhoodPageElement,
   oneDayPageElement,
+  badDaysPageElement,
 ];
 const loadSection = () => {
   for (let i = 0; i < elementsArray.length; i += 1) {
