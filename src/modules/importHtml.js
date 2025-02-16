133c73fb32dd9ec5d2c244bedda5aa51ec8fbf33
Fastexcision <atlee@yandex.ru>
2025-02-16 15:42:33 +0300

    feat: last page complete

--- src/modules/importHtml.js
+++ src/modules/importHtml.js
@@ -4,12 +4,16 @@ import titlePage from '../pages/title-page.html';
 import childhoodPage from '../pages/childhood-page.html';
 import oneDayPage from '../pages/one-day-page.html';
 import badDaysPage from '../pages/bad-days-page.html';
+import scaredOutsidePage from '../pages/scared-outside-page.html';
+import superheroPage from '../pages/superhero-page.html';
 
 const headerElement = document.getElementById('header');
 const titlePageElement = document.getElementById('title-page');
 const childhoodPageElement = document.getElementById('childhood-page');
 const oneDayPageElement = document.getElementById('one-day-page');
 const badDaysPageElement = document.getElementById('bad-days-page');
+const scaredOutsidePageElement = document.getElementById('scared-outside-page');
+const superheroPageElement = document.getElementById('superhero-page');
 
 const importPagesArray = [
   header,
@@ -17,6 +21,8 @@ const importPagesArray = [
   childhoodPage,
   oneDayPage,
   badDaysPage,
+  scaredOutsidePage,
+  superheroPage,
 ];
 const elementsArray = [
   headerElement,
@@ -24,6 +30,8 @@ const elementsArray = [
   childhoodPageElement,
   oneDayPageElement,
   badDaysPageElement,
+  scaredOutsidePageElement,
+  superheroPageElement,
 ];
 const loadSection = () => {
   for (let i = 0; i < elementsArray.length; i += 1) {
